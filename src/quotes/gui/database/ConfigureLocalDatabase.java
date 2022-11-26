8d2dea4143243217f4b7b7e9b78e69519f60e050
Kolos Kis-Bogdán <kisbogdan.kolos@gmail.com>
2022-11-26 19:34:38 +0100

    improved database file selection

--- src/quotes/gui/database/ConfigureLocalDatabase.java
+++ src/quotes/gui/database/ConfigureLocalDatabase.java
@@ -2,11 +2,15 @@ package quotes.gui.database;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import quotes.database.LocalDatabase;
 import quotes.database.LocalDatabase.LocalDatabaseConfigurator;
@@ -14,29 +18,65 @@ import quotes.database.LocalDatabase.LocalDatabaseConfigurator;
 public class ConfigureLocalDatabase extends JFrame implements LocalDatabaseConfigurator {
     private LocalDatabase db;
 
-    private JTextField tf;
+    private JLabel label;
+    private JFileChooser fc;
+    private String filename;
+    private String path;
 
     public ConfigureLocalDatabase() {
-        super("Fájlnév megadása");
-        setSize(new Dimension(300, 100));
+        super("Helyi adatábzis beállítása");
+        setSize(new Dimension(350, 100));
 
-        tf = new JTextField(10);
-        tf.setMaximumSize(new Dimension(250, 25));
-        add(tf, BorderLayout.CENTER);
+        filename = "";
+        path = System.getProperty("user.dir");
 
-        JButton bt = new JButton("OK");
-        add(bt, BorderLayout.SOUTH);
-        bt.addActionListener(e -> {
+        initComponents();
+    }
+
+    private void initComponents() {
+        fc = new JFileChooser();
+
+        label = new JLabel("Jelenlegi fájl: -");
+        label.setBorder(new EmptyBorder(10, 10, 10, 10));
+        add(label, BorderLayout.CENTER);
+
+        JPanel p = new JPanel();
+
+        JButton bt0 = new JButton("Mentés");
+        bt0.addActionListener(e -> {
             setVisible(false);
             if (db != null)
-                db.setFilename(tf.getText());
+                db.setFilename(filename);
         });
+        p.add(bt0);
+
+        JButton bt1 = new JButton("Fájl kiválasztása");
+        bt1.addActionListener(e -> {
+            int result = fc.showOpenDialog(this);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                filename = fc.getSelectedFile().getPath();
+                if (filename.startsWith(path)) {
+                    filename = filename.substring(path.length() + 1);
+                }
+                label.setText("Jelenlegi fájl: " + filename);
+            }
+        });
+        p.add(bt1);
+
+        JButton bt2 = new JButton("Mégse");
+        bt2.addActionListener(e -> setVisible(false));
+        p.add(bt2);
+
+        add(p, BorderLayout.SOUTH);
     }
 
     public void configure() {
         setVisible(true);
-        if (db != null)
-            tf.setText(db.getFilename());
+        if (db != null) {
+            filename = db.getFilename();
+            label.setText("Jelenlegi fájl: " + filename);
+            fc.setSelectedFile(new File(path, filename));
+        }
     }
 
     @Override
